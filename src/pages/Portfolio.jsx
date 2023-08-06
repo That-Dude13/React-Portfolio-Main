9ed460ff0e77182beae2268f6bbb5f1be402fefd
Change Agent <keasiley@icloud.com>
2023-08-06 17:58:28 -0500

    changes made to portfoli.js removed. Commented out code removed from Home.js

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import Navigation from '../components/Navigation';
 import NavTabs from '../components/NavTabs';
 
 const Portfolio = () => {
-  const [currentPage, setCurrentPage] = useState('Portfolio');
+const [currentPage, setCurrentPage] = useState('Portfolio');
 
+return (
+  <div>
+    <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+    <NavTabs currentPage={currentPage} setCurrentPage={setCurrentPage} />
 
-  return (
-    <div>
-    
-      {/* <main>{<Portfolio> </Portfolio>}</main> */}
-    </div>
-  );
+<main>
+<Portfolio>
+<a href="https://byte-bridge-044c48f5039e.herokuapp.com/" >
+  <h1>Portfolio</h1>
+</a>
+<h1>Byte-Bridge</h1>
+<a href img = "./components/images/byte-bridge.jpg" alt="byte-bridge">
+</a>
+</Portfolio>
+</main>
+</div>
+);
 };
 
 export default Portfolio;
