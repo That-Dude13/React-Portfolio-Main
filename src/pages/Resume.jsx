4e402f34517120b3cea3e05be4d87ab9e67b3b4b
Change Agent <keasiley@icloud.com>
2023-08-19 12:21:14 -0500

    footer added to Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import {useState} from 'react';
 import Navigation from '../components/Navigation';
 import NavTabs from '../components/NavTabs';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+
 
 
 const Resume = () => {
@@ -12,6 +19,60 @@ const Resume = () => {
     <div>
      
       {/* <main>{<Resume> </Resume>}</main> */}
+      <MDBFooter className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
+      <MDBContainer className='pt-4'>
+        <section className='mb-4'>
+          
+
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="lg"
+            className='text-dark m-1'
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab className='fa-stack-overflow' />
+          </MDBBtn>
+
+        
+
+         
+
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="lg"
+            className='text-dark m-1'
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab className='fa-linkedin' />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="lg"
+            className='text-dark m-1'
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab className='fa-github' />
+          </MDBBtn>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2020 Copyright:
+        <a className='text-dark' href='https://mdbootstrap.com/'>
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
     </div>
   );
 };
